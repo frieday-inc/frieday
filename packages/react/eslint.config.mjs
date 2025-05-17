0181fd0af6c4146398ec691bf9fbc3ebb52b9447
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-17 09:45:25 +0700

    refactor: Format code with Prettier

--- packages/react/eslint.config.mjs
+++ packages/react/eslint.config.mjs
@@ -3,8 +3,12 @@ import { fileURLToPath } from 'url';
 
 import parserTs from '@typescript-eslint/parser';
 import pluginImport from 'eslint-plugin-import';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 import pluginSonarjs from 'eslint-plugin-sonarjs';
 import pluginTs from '@typescript-eslint/eslint-plugin';
+import pluginTurbo from 'eslint-plugin-turbo';
 import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,16 +19,38 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'),
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:turbo/recommended',
+    'plugin:storybook/recommended',
+  ),
   {
     plugins: {
       '@typescript-eslint': pluginTs,
+      'jsx-a11y': pluginJsxA11y,
+      'react-hooks': pluginReactHooks,
       import: pluginImport,
+      react: pluginReact,
       sonarjs: pluginSonarjs,
+      turbo: pluginTurbo,
     },
     rules: {
       ...pluginSonarjs.configs.recommended.rules,
-      'prettier/prettier': 'warn',
+      'prettier/prettier': 'error',
+      'react/jsx-props-no-spreading': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
       'import/no-extraneous-dependencies': [
         'error',
         {
@@ -52,6 +78,9 @@ const eslintConfig = [
       ],
     },
     settings: {
+      react: {
+        version: 'detect',
+      },
       'import/resolver': {
         node: {
           extensions: ['.js', '.jsx', '.ts', '.tsx'],
@@ -64,17 +93,17 @@ const eslintConfig = [
     },
   },
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       parser: parserTs,
       parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
         ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
       },
     },
   },
