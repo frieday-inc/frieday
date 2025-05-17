28bd7c909ba6e9d7f17f88826dadd29c59b7dade
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-17 22:24:14 +0700

    test

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,17 @@
 import type { UserConfig } from '@commitlint/types';
 
-const config: UserConfig = {
+const commitlintConfig: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  helpUrl: 'https://www.conventionalcommits.org/en/v1.0.0',
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'build', 'test', 'ci', 'chore', 'docs', 'style', 'perf', 'revert'],
+    ],
+  },
 };
 
-export default config;
+export default commitlintConfig;
