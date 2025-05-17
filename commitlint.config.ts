3fef50f4f71554aacd79f9fe82c8bfb6e8cd23c9
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-17 22:27:01 +0700

    test

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,12 +5,15 @@ const commitlintConfig: UserConfig = {
   helpUrl: 'https://www.conventionalcommits.org/en/v1.0.0',
   rules: {
     'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
     'subject-case': [2, 'always', 'sentence-case'],
     'type-enum': [
       2,
       'always',
       ['feat', 'fix', 'refactor', 'build', 'test', 'ci', 'chore', 'docs', 'style', 'perf', 'revert'],
     ],
+    'header-min-length': [2, 'always', 10],
   },
 };
 
