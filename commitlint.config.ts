23217a4dad204590c19eb46e5bec9eac2efa0f62
Joe Takara <44985154+joetakara@users.noreply.github.com>
2025-05-19 02:37:50 +0700

    Chore/setup actions (#39)
    test

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,11 +5,12 @@ const commitlintConfig: UserConfig = {
   helpUrl: 'https://www.conventionalcommits.org/en/v1.0.0',
   rules: {
     'type-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'always', '.'],
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'refactor', 'build', 'test', 'ci', 'chore', 'docs', 'style', 'perf', 'revert'],
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
     ],
   },
 };
