3fa246246c75fd029672fc6d9be34895ff948016
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-19 02:02:55 +0700

    chore: add message-lowercase rule

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,7 +5,7 @@ const commitlintConfig: UserConfig = {
   helpUrl: 'https://www.conventionalcommits.org/en/v1.0.0',
   rules: {
     'type-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-case': [2, 'always', 'lower-case'],
     'type-enum': [
       2,
       'always',
